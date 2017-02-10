4f9e4e919e771a2674816c7e94ad69a4e81c102b
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-02-10 12:37:57 +0100

    Finished Lecture 16

--- Lecture14/beginning-of-lecture-code/app.js
+++ Lecture14/beginning-of-lecture-code/app.js
@@ -6,10 +6,36 @@ angular.module('CounterApp', [])
 
 CounterController.$inject = ['$scope'];
 function CounterController($scope) {
+  $scope.onceCounter = 0;
+  $scope.counter = 0;
+  $scope.name = "Yaakov";
 
   $scope.showNumberOfWatchers = function () {
+    console.log('# of Watchers', $scope.$$watchersCount)
 
   };
+
+  $scope.countOnce = function () {
+    $scope.onceCounter = 1;
+  };
+
+  $scope.upCounter = function () {
+    $scope.counter++;
+  }
+
+  $scope.$watch(function () {
+    console.log("Digest loop Fired!")
+  })
+
+  // $scope.$watch('onceCounter', function (newValue, oldValue) {
+  //   console.log("old value:", oldValue);
+  //   console.log('new value', newValue);
+  // });
+  //
+  // $scope.$watch('counter', function (newValue, oldValue) {
+  //   console.log("counter old value:", oldValue);
+  //   console.log('counter new value', newValue);
+  // });
 
 }
 
