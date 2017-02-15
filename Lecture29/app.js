39dfcc6303f348731233a47415a0f1c5b72288e6
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-02-15 11:24:09 +0100

    Finished Lecture 32

--- Lecture29/app.js
+++ Lecture29/app.js
@@ -4,7 +4,7 @@
 angular.module('ShoppingListDirectiveApp', [])
 .controller('ShoppingListController', ShoppingListController)
 .factory('ShoppingListFactory', ShoppingListFactory)
-// .controller('ShoppingListDirectiveController', ShoppingListDirectiveController)
+.controller('ShoppingListDirectiveController', ShoppingListDirectiveController)
 .directive('shoppingList', ShoppingListDirective);
 
 
@@ -16,8 +16,8 @@ function ShoppingListDirective() {
       title: '@'
     },
     // controller: 'ShoppingListDirectiveController as list',
-    controller: ShoppingListDirectiveController,
-    controllerAs: 'list',
+    controller: 'ShoppingListDirectiveController as list',
+    //controllerAs: 'list',
     bindToController: true
   };
 
@@ -35,7 +35,6 @@ function ShoppingListDirectiveController() {
         return true;
       }
     }
-
     return false;
   };
 }
