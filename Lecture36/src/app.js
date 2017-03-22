ca8b633951934e01e27ae28e524f1c33e9f6f612
kelomaniack <e.konstantinidis@app-smart.de>
2017-03-22 18:14:21 +0100

    Finished Lecture 45

--- Lecture36/src/app.js
+++ Lecture36/src/app.js
@@ -21,7 +21,23 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
     .state('tab2', {
       url: '/tab2',
       templateUrl: 'src/tab2.html'
-    });
+    })
+
+
+
+  // $urlRouterProvider.otherwise('/tab1');
+  //
+  // // Set up UI states
+  // $stateProvider
+  //   .state('tab1', {
+  //     url: '/tab1',
+  //     templateUrl: 'src/tab1.html'
+  //   })
+  //
+  //   .state('tab2', {
+  //     url: '/tab2',
+  //     templateUrl: 'src/tab2.html'
+  //   });
 }
 
 
