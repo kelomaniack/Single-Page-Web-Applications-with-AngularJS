901d6c0ebd2cce3c44d649da843d91d444d3cc38
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-06-28 15:55:46 +0200

    changes

--- Assignments solutions/assignment4/src/routes.js
+++ Assignments solutions/assignment4/src/routes.js
@@ -1,45 +1,44 @@
-(function () {
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('MenuApp')
-.config(RoutesConfig);
+    angular.module('MenuApp')
+        .config(RoutesConfig);
 
-RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-function RoutesConfig($stateProvider, $urlRouterProvider) {
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-  // Redirect to home page if no other URL matches
-  $urlRouterProvider.otherwise('/');
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
 
-  // *** Set up UI states ***
-  $stateProvider
-  // Home page
-  .state('home', {
-    url: '/',
-    templateUrl: 'src/menu/templates/home.template.html'
-  })
-  // Categories list page
-  .state('categories', {
-    url: '/categories',
-    templateUrl: 'src/menu/templates/categories.template.html',
-    controller: 'CategoriesController as categoriesCtrl',
-    resolve: {
-      items: ['MenuDataService', function (MenuDataService) {
-        return MenuDataService.getAllCategories();
-      }]
+        // Redirect to home page if no other URL matches
+        $urlRouterProvider.otherwise('/');
+
+        // *** Set up UI states ***
+        $stateProvider
+            // Home page
+            .state('home', {
+                url: '/',
+                templateUrl: 'src/menu/templates/home.template.html'
+            })
+            // Categories list page
+            .state('categories', {
+                url: '/categories',
+                templateUrl: 'src/menu/templates/categories.template.html',
+                controller: 'CategoriesController as categoriesCtrl',
+                resolve: {
+                    items: ['MenuDataService', function(MenuDataService) {
+                        return MenuDataService.getAllCategories();
+                    }]
+                }
+            })
+            // Items list page
+            .state('categories.items', {
+                url: '/{categoryShortName}/items',
+                templateUrl: 'src/menu/templates/items.template.html',
+                controller: 'ItemsController as itemsCtrl',
+                resolve: {
+                    items: ['$stateParams', 'MenuDataService', function($stateParams, MenuDataService) {
+                        return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+                    }]
+                }
+            });
     }
-  })
-  // Items list page
-  .state('categories.items', {
-     url: '/{categoryShortName}/items',
-     templateUrl: 'src/menu/templates/items.template.html',
-     controller: 'ItemsController as itemsCtrl',
-     resolve: {
-       items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
-         return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
-       }]
-     }
-});
-
-}
-
 })();
